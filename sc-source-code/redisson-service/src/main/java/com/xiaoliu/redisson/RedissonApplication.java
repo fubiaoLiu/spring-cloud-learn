5a88d60d95d47030c604129febf6ac9ec20935c3
Fubiao.Liu <liufubiao@vip.qq.com>
2020-09-06 21:41:13 +0800

    feat: 本地代码提交

--- sc-source-code/redisson-service/src/main/java/com/xiaoliu/redisson/RedissonApplication.java
+++ sc-source-code/redisson-service/src/main/java/com/xiaoliu/redisson/RedissonApplication.java
@@ -7,15 +7,72 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
 /**
- * @description: 事务服务启动类
+ * @description: Redisson Demo
  * @author: liufb
  * @create: 2020/7/31 11:10
  **/
-//@SpringBootApplication
 public class RedissonApplication {
-    public static void main(String[] args) {
-//        SpringApplication.run(RedissonApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        // singleServer();
+        // clusterServerReentrantLock();
+        clusterServerFairLock();
+    }
 
+    private static RedissonClient getRedissonClient() {
+        // 1. Create config object
+        Config config = new Config();
+        config.useClusterServers()
+                // use "rediss://" for SSL connection
+                .addNodeAddress("redis://192.168.2.141:7001")
+                .addNodeAddress("redis://192.168.2.141:7002")
+                .addNodeAddress("redis://192.168.2.141:7003")
+                .addNodeAddress("redis://192.168.2.134:7001")
+                .addNodeAddress("redis://192.168.2.134:7002")
+                .addNodeAddress("redis://192.168.2.134:7003");
+
+        // or read config from file
+        // config = Config.fromYAML(new File("config-file.yaml"));
+
+        // 2. Create Redisson instance
+        // Sync and Async API
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+
+    /**
+     * 基于redis cluster集群的公平锁
+     *
+     * @throws Exception
+     */
+    private static void clusterServerFairLock() throws Exception {
+        RedissonClient redisson = getRedissonClient();
+        RLock fairLock = redisson.getFairLock("anyLock");
+
+        fairLock.lock();
+        Thread.sleep(3600000);
+        fairLock.unlock();
+    }
+
+    /**
+     * 基于redis cluster集群的可重入锁
+     *
+     * @throws Exception
+     */
+    private static void clusterServerReentrantLock() throws Exception {
+        RedissonClient redisson = getRedissonClient();
+
+        // Get Redis based implementation of java.util.concurrent.locks.Lock
+        RLock lock = redisson.getLock("myLock");
+
+        lock.lock();
+        Thread.sleep(3600000);
+        lock.unlock();
+    }
+
+    /**
+     * 基于redis单实例简单看RedLock源码
+     */
+    private static void singleServer() {
         Config config = new Config();
         config.useSingleServer().setAddress("redis://127.0.0.1:6379");
         RedissonClient redissionClient = Redisson.create(config);
