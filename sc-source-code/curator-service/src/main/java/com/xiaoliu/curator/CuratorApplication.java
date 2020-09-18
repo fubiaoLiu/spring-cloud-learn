476e46991a344a0c46a53ea2189347c716fbf28e
Fubiao.Liu <liufubiao@vip.qq.com>
2020-09-18 21:07:23 +0800

    feat: curator-service代码提交

--- sc-source-code/curator-service/src/main/java/com/xiaoliu/curator/CuratorApplication.java
+++ sc-source-code/curator-service/src/main/java/com/xiaoliu/curator/CuratorApplication.java
@@ -4,6 +4,7 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.framework.recipes.locks.InterProcessSemaphoreMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
 /**
@@ -14,9 +15,30 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 public class CuratorApplication {
     public static void main(String[] args) throws Exception {
         // testZookeeperNormal();
-        reentrantLock();
+        // reentrantLock();
+        semaphore();
     }
 
+    /**
+     * semaphore
+     *
+     * @throws Exception
+     */
+    private static void semaphore() throws Exception {
+        CuratorFramework client = getClient();
+        String path = "/locks/lock_01";
+
+        InterProcessSemaphoreMutex lock = new InterProcessSemaphoreMutex(client, path);
+        lock.acquire();
+        Thread.sleep(1000);
+        lock.release();
+    }
+
+    /**
+     * 可重入锁
+     *
+     * @throws Exception
+     */
     private static void reentrantLock() throws Exception {
         CuratorFramework client = getClient();
         String path = "/locks/lock_01";
